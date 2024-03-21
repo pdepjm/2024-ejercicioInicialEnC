3163cbe65f08139bc5ae2f30f35fe126e26ed2be
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2024-03-21 11:34:33 -0300

    Mejoro expresividad

--- bobEsponja.c
+++ bobEsponja.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void bobEsponja(int a[], int b, int c[]){
-  int d, e = 0;  
-  for (d = 0; d < b; d++) { 
-    if (a[d] % 2 == 0) { 
-        c[e] = a[d]; 
-        e = e + 1; 
+void pares(int entrada[], int cantidadElementos, int salida[]){
+  int i, j = 0;  
+  for (i = 0; i < cantidadElementos; i++) { 
+    if (entrada[i] % 2 == 0) { 
+        salida[j] = entrada[i]; 
+        j = j + 1; 
     }
 }
 }
@@ -13,6 +13,6 @@ void bobEsponja(int a[], int b, int c[]){
 void main(){
     int entrada[] = {1,2,3,4,5,6,7,8,9};
     int salida[] = {0,0,0,0,0,0,0,0,0};
-    bobEsponja(entrada, 9, salida);
+    pares(entrada, 9, salida);
     printf("Salida: [%d,%d,%d,%d,%d,%d,%d,%d,%d]\n", salida[0], salida[1], salida[2], salida[3], salida[4], salida[5], salida[6], salida[7], salida[8]);
 }
